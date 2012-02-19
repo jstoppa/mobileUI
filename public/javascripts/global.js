6bc4becbe273bc1e6b3eca6ec881c54b3836bc7f
Juan Stoppa <jstoppa@gmail.com>
2012-02-19 17:24:17 +0000

    adding jqtouch functionality

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,6 +1,9 @@
-var jQT = new $.jQTouch({
-   icon: 'jqtouch.png',
-   addGlossToIcon: true,
-   startupScreen: 'jqt_startup.png',
-   statusBar: 'black',
-   preloadImages: []
+function initialize() {
+	var myOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            myOptions);
+}
